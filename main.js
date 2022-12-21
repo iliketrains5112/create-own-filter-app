738ab1dcdd7b1faf2f2620ec0596d8356b2c4c4b
iliketrains <84407492+iliketrains5112@users.noreply.github.com>
2022-12-21 16:36:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,14 +3,31 @@ function preload(){
 }
 
 function setup(){
-    canvas=createCanvas(300, 300);
+    canvas=createCanvas(500, 500);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw(){
-
+image(video, 0, 0, 500, 500);
 }
 
 function takeSnapshot(){
-    save("clowned_image")
+    save("filtered_image");
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results){
+if (results.length > 0 ){
+    console.log(results)
+    console.log("Right Ear x pos = " + results[0].pose.rightEar.x);
+    console.log("Right Ear y pos = " + results[0].pose.rightEar.y);
+}
 }
