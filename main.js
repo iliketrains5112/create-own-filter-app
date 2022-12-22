a397bddcf7ab1615c50d5fb412ad6a2c224b1992
iliketrains <84407492+iliketrains5112@users.noreply.github.com>
2022-12-22 17:23:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-function preload(){
+rightEarY = 0;
+rightEarX= 0;
 
+
+function preload(){
+shrek = loadImage('https://i.postimg.cc/kMWZPSMh/shrek.png');
 }
 
 function setup(){
     canvas=createCanvas(500, 500);
     canvas.center();
     video = createCapture(VIDEO);
+    video.size(500, 500);
     video.hide();
 
     poseNet=ml5.poseNet(video, modelLoaded);
@@ -14,10 +19,11 @@ function setup(){
 
 function draw(){
 image(video, 0, 0, 500, 500);
+image(shrek, rightEarX-50, rightEarY-150, 300, 300);
 }
 
 function takeSnapshot(){
-    save("filtered_image");
+    save("shrekifiedimage.png");
 }
 
 function modelLoaded(){
@@ -29,5 +35,7 @@ if (results.length > 0 ){
     console.log(results)
     console.log("Right Ear x pos = " + results[0].pose.rightEar.x);
     console.log("Right Ear y pos = " + results[0].pose.rightEar.y);
+    rightEarX = results[0].pose.rightEar.x;
+    rightEarY = results[0].pose.rightEar.y;
 }
 }
